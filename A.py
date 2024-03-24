77f9d21b42ecb38e2a27174b494afb5da194cbd7
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-03-25 00:16:33 +0800

    Update A.py

--- A.py
+++ A.py
@@ -11,27 +11,35 @@ def get_server_time():
 def generate_signature(data, secret_key):
     return hmac.new(secret_key.encode('utf-8'), data.encode('utf-8'), hashlib.sha256).hexdigest()
 
-def get_crypto_price(symbol, timestamp, api_key, secret_key):
-    endpoint = f'https://api.binance.com/api/v3/klines?symbol={symbol}USDT&interval=1d&startTime={timestamp}&limit=1'
-    headers = {
-        'X-MBX-APIKEY': api_key,
-    }
-    response = requests.get(endpoint, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        if len(data) > 0:
-            price = float(data[0][4])  # Mengambil harga penutup
-            return price
+def get_crypto_price(symbol, timestamps, api_key, secret_key):
+    prices = {}
+    for timestamp in timestamps:
+        endpoint = f'https://api.binance.com/api/v3/klines?symbol={symbol}USDT&interval=1d&startTime={timestamp}&limit=1'
+        headers = {
+            'X-MBX-APIKEY': api_key,
+        }
+        response = requests.get(endpoint, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            if len(data) > 0:
+                price = float(data[0][4])  # Mengambil harga penutup
+                prices[timestamp] = price
+            else:
+                prices[timestamp] = f"Tidak dapat menemukan data harga untuk simbol {symbol}"
         else:
-            return f"Tidak dapat menemukan data harga untuk simbol {symbol}"
-    else:
-        return f"Permintaan gagal dengan kode status {response.status_code}"
+            prices[timestamp] = f"Permintaan gagal dengan kode status {response.status_code}"
+    return prices
 
 # Contoh penggunaan:
 symbols = ['BTC', 'BNB', 'ADA', 'DOT', 'SOL', 'XRP', 'DOGE', 'XLM']
-timestamp = get_server_time() - (86400000 * 30)  # Mendapatkan waktu server dan mengurangkan 30 hari (dalam milidetik)
+tanggal1 = '2020-05-11'
+tanggal2 = '2021-04-11'
+timestamps = [int(time.mktime(time.strptime(tanggal1, '%Y-%m-%d'))) * 1000, int(time.mktime(time.strptime(tanggal2, '%Y-%m-%d'))) * 1000]
 api_key = 'knkGeFTOXDxdUPQ783FvyuLKL6KcIBYHwIGaczIyDXvQSipzJToxKI9J7qLyttLT'  # Ganti dengan API key Binance Anda
 secret_key = 'pa04RmmpZZmI4u83EWbLMy8kguJLxEeQtcQjfJBIUKJIZ6SEjAMsAiBmblXaKzJe'  # Ganti dengan Secret key Binance Anda
+
 for symbol in symbols:
-    harga = get_crypto_price(symbol, timestamp, api_key, secret_key)
-    print(f'Harga {symbol} pada tanggal {time.strftime("%Y-%m-%d", time.gmtime(timestamp // 1000))}: ${harga}')
+    harga = get_crypto_price(symbol, timestamps, api_key, secret_key)
+    print(f'Harga {symbol}:')
+    for timestamp, price in harga.items():
+        print(f'- Tanggal {time.strftime("%Y-%m-%d", time.gmtime(timestamp // 1000))}: ${price}')
