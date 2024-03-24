72b623d39e6198b23fa498ec32dbe05fd0b3cc20
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-03-24 23:59:27 +0800

    Update A.py

--- A.py
+++ A.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def get_crypto_prices(symbols, date):
     api_key = 'dbe9d93e-6a86-47ff-87a4-2fd7d73fcfc4'  # Ganti dengan kunci API Anda
-    date_str = date.strftime('%Y-%m-%d')
+    date_str = date.strftime('%Y%m%d')
     url = f'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/historical?symbol={",".join(symbols)}&time_start={date_str}&time_end={date_str}&convert=USD'
     headers = {
         'Accepts': 'application/json',
